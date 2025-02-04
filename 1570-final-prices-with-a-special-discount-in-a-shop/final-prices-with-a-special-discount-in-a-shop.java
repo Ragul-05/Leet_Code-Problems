beba7395c28d0f2c0588ea8e01e1aa254b73a6a6
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-02-04 23:30:53 +0530

    Time: 3 ms (37.01%) | Memory: 43.7 MB (53.39%) - LeetSync

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] res = prices.clone();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=0;i<prices.length;i++){
+            while(!stack.isEmpty() && prices[stack.peek()] >= prices[i]){
+                res[stack.pop()] -= prices[i];
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}

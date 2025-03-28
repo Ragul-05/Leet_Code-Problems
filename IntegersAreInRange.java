6d7a3feda288cc6d4b0097fa7422279cb77a4c42
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-03-28 20:15:20 +0530

    Problem no -->1893

---
+++ IntegersAreInRange.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        boolean[] covered = new boolean[51];
+        
+        for (int[] range : ranges) {
+            int start = range[0];
+            int end = range[1];
+            
+            for (int i = start; i <= end; i++) {
+                covered[i] = true;
+            }
+        }
+        
+        for (int i = left; i <= right; i++) {
+            if (!covered[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}

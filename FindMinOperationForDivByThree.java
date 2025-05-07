29f6bd58f0ff700394af8dae81a670e127e850b2
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-05-07 22:59:59 +0530

    Problem no -->3190

---
+++ FindMinOperationForDivByThree.java
@@ -0,0 +1,13 @@
+package Leet_Code_Problems;
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int sum = 0;
+        for(int i = 0 ; i < nums.length;i++){
+            if(nums[i] % 3 != 0){
+                sum++;
+            }
+        }
+        return sum;
+    }
+}

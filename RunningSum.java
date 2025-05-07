d9d1735b725f578aafb1259d8d7a36c40425f220
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-05-07 23:08:59 +0530

    Problem no -->1480

---
+++ RunningSum.java
@@ -0,0 +1,11 @@
+package Leet_Code_Problems;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        for(int i = 1;i < n ; i++){
+            nums[i] = nums[i-1]+nums[i];
+        }
+        return nums;
+    }
+}

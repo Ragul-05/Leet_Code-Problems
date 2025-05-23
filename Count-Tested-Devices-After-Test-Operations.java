d7cd018a43cad62e6e314d25ed421ed086fd43a4
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-05-23 23:22:33 +0530

    [2960] [Time Beats: 100.00%] [Memory Beats: 95.09%] - LeetPush

--- Count-Tested-Devices-After-Test-Operations.java
+++ Count-Tested-Devices-After-Test-Operations.java
@@ -1,7 +1,11 @@
-class Solution{
-public int countTestedDevices(int[] batteryPercentages) {
-    int ans = 0;
-    for(int b: batteryPercentages) ans += (b > ans)?1:0;
-    return ans;
-}
+class Solution {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int ans =0;
+        for(int i=0;i<batteryPercentages.length;i++){
+            if(batteryPercentages[i]-ans>0){
+                ans++;
+            }
+        }
+        return ans;        
+    }
 }

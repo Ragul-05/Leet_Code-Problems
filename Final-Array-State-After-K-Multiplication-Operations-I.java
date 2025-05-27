58374217ed74b2ecb62000a85ff9cfa1f3f4a36a
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-05-27 21:58:59 +0530

    [3264] [Time Beats: 100.00%] [Memory Beats: 72.57%] - LeetPush

--- Final-Array-State-After-K-Multiplication-Operations-I.java
+++ Final-Array-State-After-K-Multiplication-Operations-I.java
@@ -1,17 +1,21 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        while(k>0){
-            int min=Integer.MAX_VALUE;
-            int minIndex=-1;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]<min){
-                    min=nums[i];
-                    minIndex=i;
+        for (int i = 0; i < k; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < nums.length; j++) {
+                if (min > nums[j]) {
+                    min = nums[j]; // Update the minimum value found
                 }
             }
-            nums[minIndex]=min*multiplier;
-            k--;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (min == nums[j]) {
+                    nums[j] *= multiplier;  // Multiply the smallest element by 'multiplier'
+                    break;
+                }
+            }
         }
+
         return nums;
     }
 }

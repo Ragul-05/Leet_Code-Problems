1ea7bf2b05b8e8d7a7b771ed419458fdb76911d2
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-05-29 22:40:37 +0530

    [9] [Time Beats: 69.98%] [Memory Beats: 49.53%] - LeetPush

---
+++ Palindrome-Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int revno = 0;
+        int dup = x;
+        while(x>0){
+            int ld = x % 10;
+            revno = (revno * 10)+ ld;
+            x =x/10;
+        }
+        if(dup == (revno)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

1cd43a5037f8fe472c65b190982d949bf7a91e47
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-06-01 21:39:24 +0530

    [1662] [Time Beats: 62.69%] [Memory Beats: 27.42%] - LeetPush

--- Check-If-Two-String-Arrays-are-Equivalent.java
+++ Check-If-Two-String-Arrays-are-Equivalent.java
@@ -1,20 +1,7 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-       StringBuilder sb = new StringBuilder("");
-        StringBuilder sb2 = new StringBuilder("");
-
-        for(int i = 0; i < word1.length; i++) {
-            sb.append(word1[i]);
-        }
-
-        for(int j = 0; j < word2.length; j++) {
-            sb2.append(word2[j]);
-        }
-
-        if(sb.toString().equals(sb2.toString())){
-            return true;
-        } else {
-            return false;
-        }
+        String s1 = String.join("", word1);
+        String s2 = String.join("", word2);
+        return s1.equals(s2);
     }
 }

57fd479a81368da81a1c18fd972ba1afaeea923d
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2025-07-28 22:46:34 +0530

    [2485] [Time Beats: 100.00%] [Memory Beats: 63.89%] - LeetPush

--- Find-the-Pivot-Integer.java
+++ Find-the-Pivot-Integer.java
@@ -1,11 +1,17 @@
 class Solution {
-    public int pivotInteger(int n) {
-    for (int x = 1; x <= n; x++) {
-        int left = x * (x + 1) / 2;
-        int right = (n * (n + 1) / 2) - ((x - 1) * x / 2);
-        if (left == right) return x;
-    }
-    return -1;
-}
+   public int pivotInteger(int n) {
+    int left = 1, right = n;
+    int sumLeft = 1, sumRight = n;
 
+    while (left < right) {
+        if (sumLeft < sumRight) {
+            left++;
+            sumLeft += left;
+        } else {
+            right--;
+            sumRight += right;
+        }
+    }
+    return (sumLeft == sumRight) ? left : -1;
+}
 }

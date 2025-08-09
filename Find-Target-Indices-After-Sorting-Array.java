d4b8badb46e65ae02e0909a27e9419bffa0a57b0
RAGUL MKG <ragulgopinath2005@gmail.com>
2025-08-09 23:55:10 +0530

    [2089] [Time Beats: 100.00%] [Memory Beats: 64.34%] - LeetPush

--- Find-Target-Indices-After-Sorting-Array.java
+++ Find-Target-Indices-After-Sorting-Array.java
@@ -1,11 +1,13 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         List<Integer> result = new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i = 0 ; i < nums.length; i++){
-            if(nums[i] == target){
-                result.add(i);
-            }
+        int lessCount = 0, equalCount = 0;
+        for (int num : nums) {
+            if (num < target) lessCount++;
+            else if (num == target) equalCount++;
+        }
+        for (int i = 0; i < equalCount; i++) {
+            result.add(lessCount + i);
         }
         return result;
     }

bc3e03b2b1b9f91618f04241b5d5708d2325660f
RAGUL MKG <ragulgopinath2005@gmail.com>
2025-09-02 23:36:59 +0530

    [108] [Time Beats: 100.00%] [Memory Beats: 74.02%] - LeetPush

---
+++ Convert-Sorted-Array-to-Binary-Search-Tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        int n = nums.length;
+        return helper(nums,0,n-1);
+    }
+    TreeNode helper(int[] a , int left,int right){
+        if(left > right){
+            return null;
+        }
+        int mid = (left + right) / 2;
+        TreeNode nn = new TreeNode(a[mid]);
+        nn.left = helper(a,left,mid-1);
+        nn.right = helper(a,mid+1,right);
+        return nn;
+    }
+}

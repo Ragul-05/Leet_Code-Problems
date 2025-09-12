deeffe55087bd80ceb46328a472d34d29098906d
RAGUL MKG <ragulgopinath2005@gmail.com>
2025-09-12 21:18:08 +0530

    [2443] [Time Beats: 33.26%] [Memory Beats: 100.00%] - LeetPush

---
+++ Sum-of-Number-and-Its-Reverse.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean sumOfNumberAndReverse(int num) {
+        for (int x = 0; x <= num; x++) {
+            int rev = 0, temp = x;
+            while (temp > 0) {
+                rev = rev * 10 + temp % 10;
+                temp /= 10;
+            }
+            if (x + rev == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

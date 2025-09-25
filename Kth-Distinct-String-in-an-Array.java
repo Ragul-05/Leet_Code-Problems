6b2b8216f72f3f1b44ccbee7174c56eeaf1848d5
RAGUL MKG <ragulgopinath2005@gmail.com>
2025-09-25 23:25:14 +0530

    [2053] [Time Beats: 49.54%] [Memory Beats: 85.70%] - LeetPush

--- Kth-Distinct-String-in-an-Array.java
+++ Kth-Distinct-String-in-an-Array.java
@@ -1,16 +1,19 @@
+import java.util.*;
+
 class Solution {
     public String kthDistinct(String[] arr, int k) {
-        Map<String,Integer> hm = new HashMap<>();
-        for(String s : arr){
-            hm.put(s,hm.getOrDefault(s,0)+1);
+        Map<String, Integer> hm = new LinkedHashMap<>();
+        for (String s : arr) {
+            hm.put(s, hm.getOrDefault(s, 0) + 1);
         }
         int count = 0;
-        for(String s : arr){
-            if(hm.get(s) == 1){
+        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() == 1) {
                 count++;
+                if (count == k) return entry.getKey();
             }
-            if(count == k ) return s;
         }
+
         return "";
     }
 }
